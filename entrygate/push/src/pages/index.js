138a88a589c31d73f1a5934b1f42e8f4d36afb32
Bilal Gujjar <bilaltahir515@gmail.com>
2020-11-01 12:36:29 +0500

    Netlify Identity
    Add User form

--- entrygate/push/src/pages/index.js
+++ entrygate/push/src/pages/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container,Heading,Button,Flex} from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget"
 
-export default props => (
+
+
+export default props => {
+    useEffect (()=>{
+        netlifyIdentity.init({});
+    });
+    return(
     <Container>
         <Flex sx={{flexDirection :"column",padding:30}}>
             <Heading as = "h1">Todo App</Heading>
             <Button sx ={{ marginTop:20}} 
-                onClick = {() => {alert("Login  SucessFully ")}}>
+                onClick = {() => { netlifyIdentity.open()}}>
                 Login Test PoPup
                 </Button>
         </Flex>
     </Container>
-);
+)}
